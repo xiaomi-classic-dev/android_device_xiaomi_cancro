12fe8adce322a717da1fa1bbf2271a9afce1e460
GuaiYiHu <guaiyihu@foxmail.com>
2017-10-13 23:59:18 +0800

    cancro: Add missing Seccomp policy
    Change-Id: I313737bbe5483c9ea46e30071cf09913df4ae0e8

--- cancro.mk
+++ cancro.mk
@@ -251,6 +251,10 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     context_hub.default \
