0a389b6665cd1715ff5c7274f280eff0fc571a83
GuaiYiHu <guaiyihu@foxmail.com>
2017-10-13 12:15:19 +0800

    cancro: Do not include generate_extra_images.mk
    Change-Id: Icec96e3b045afceccb56777b010a071b246588f0

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -7,7 +7,7 @@ ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 #----------------------------------------------------------------------
 # extra images
 #----------------------------------------------------------------------
-include build/core/generate_extra_images.mk
+#include build/core/generate_extra_images.mk
 
 # include the non-open-source counterpart to this file
 -include vendor/xiaomi/cancro/AndroidBoardVendor.mk
