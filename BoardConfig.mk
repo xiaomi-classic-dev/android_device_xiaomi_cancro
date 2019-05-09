ac0a9b4096797d1ffe9157a4490ebc9f939104d5
GuaiYiHu <guaiyihu@foxmail.com>
2019-05-09 12:03:39 +0800

    Update for omni pie
    Change-Id: I1f8bbe9d1789b7325c2eec323cf500f6d31040e5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,13 +21,13 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8974
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/xiaomi/cancro/dt.img
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000 --dt device/xiaomi/cancro/dt.img
 
 # fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16384000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16384000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1342177280
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 13291503000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 #kernel
@@ -38,19 +38,14 @@ LZMA_RAMDISK_TARGETS                    := boot,recovery
 TARGET_USERIMAGES_USE_EXT4              := true
 TARGET_USERIMAGES_USE_F2FS              := true
 RECOVERY_VARIANT                        := twrp
-DEVICE_RESOLUTION                       := 1080x1920
+TW_THEME                                := portrait_hdpi
 BOARD_HAS_NO_REAL_SDCARD                := true
 BOARD_RECOVERY_SWIPE                    := true
 TARGET_PREBUILT_RECOVERY_KERNEL         := $(call my-dir)/kernel
 RECOVERY_GRAPHICS_USE_LINELENGTH        := true
-TARGET_RECOVERY_PIXEL_FORMAT            := "RGBX_8888"
 TARGET_RECOVERY_FSTAB                   := device/xiaomi/cancro/recovery.fstab
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID  := true
-RECOVERY_FSTAB_VERSION                  := 2
-BOARD_NATIVE_DUALBOOT                   := true
-BOARD_NATIVE_DUALBOOT_SINGLEDATA        := true
 TARGET_RECOVERY_LCD_BACKLIGHT_PATH      := "/sys/class/leds/lcd-backlight/brightness"
-TW_INCLUDE_CRYPTO                       := true
 TW_NO_SCREEN_TIMEOUT                    := true
 TW_NO_SCREEN_BLANK                      := true
 TARGET_RECOVERY_QCOM_RTC_FIX            := true
@@ -58,3 +53,4 @@ BOARD_SUPPRESS_SECURE_ERASE             := true
 BOARD_SUPPRESS_EMMC_WIPE                := true
 RECOVERY_SDCARD_ON_DATA                 := true
 TW_EXTRA_LANGUAGES                      := true
+TW_USE_TOOLBOX                          := true
